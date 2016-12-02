d07f6b0718268bc115c70a9087e05bc1bbb89599
Gustavo Novaes <gustavonovaes93@gmail.com>
2016-12-01 22:50:54 -0300

    Return a proper response to ajax requests

--- src/error_handler.php
+++ src/error_handler.php
@@ -2,17 +2,29 @@
 
 set_exception_handler(function ($e) {
 
+    $response = [
+        'status' => 500,
+        'title' => 'Erro Interno',
+        'message' => is_dev() ? $e->getMessage(): 'No keyboard detected press f1 to continue.',
+    ];
+
     if ($e instanceof \App\Exceptions\NotFoundException) {
-        return view('error', [
+        $response = [
+            'status' => 404,
             'title' => '404 - Not Found',
-            'message' => '404',
-        ]);
+            'message' => $e->getMessage(),
+        ];
     }
 
-    view('error', [
-        'title' => 'Erro Interno',
-        'message' => 'No keyboard detected press f1 to continue.'
-    ]);
+    $is_ajax = isset($_SERVER['HTTP_X_REQUESTED_WITH']);
+
+    if ($is_ajax) {
+        header('Content-type: application/json');
+        http_response_code($response['status']);
+        die($response['message']);
+    }
+
+    view('error', $response, $response['status']);
 });
 
 set_error_handler(function ($errno, $errstr) {
